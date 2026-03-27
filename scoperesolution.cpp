dad2f1ee3756094bd04caaeeef15a718343e4c88
Om More <omkailasmore@gmail.com>
2026-03-27 14:48:01 +0530

    Add files via upload

---
+++ scoperesolution.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class A {
+public:
+    void func() {
+        cout << "I am in class A" << endl;
+    }
+};
+
+class B {
+public:
+    void func() {
+        cout << "I am in Class B" << endl;
+    }
+};
+
+class C : public A, public B {
+};
+
+int main() {
+    C obj;
+    obj.A::func();
+    obj.B::func();
+    return 0;
+}   
